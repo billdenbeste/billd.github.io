3bc07e9c60f4de8715820db0ab3def71256fe4ad
billdenbeste <billd1049@gmail.com>
2018-12-31 13:54:56 -0800

    Add files via upload

--- ox_enable.js
+++ ox_enable.js
@@ -10,7 +10,8 @@ const ENABLE_FLAG_UUID        = "17603fac-2e15-4afd-962d-107464389c5a";
 
 $(function() {
 	var requestDeviceParams = {
-		services: [ "f3e031b2-f057-4dbc-917d-8cacf6e78234" ]
+		filters: [ { name: [ "Ox 0" ] } ],
+		optionalServices: [ "f3e031b2-f057-4dbc-917d-8cacf6e78234" ]
 	};
 
 	$("#connect").click(() => {
