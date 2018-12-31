c0baf59ddafedae11ace538b8d1a00bf8ec3a288
billdenbeste <billd1049@gmail.com>
2018-12-31 11:52:59 -0800

    Add files via upload

--- ox_enable.js
+++ ox_enable.js
@@ -7,11 +7,13 @@ const XBATTV_CHAR_UUID        = "beb5483e-36e1-4688-b7f5-ea07361b26a8";
 const IBATTV_CHAR_UUID        = "be7a45e5-4c37-40cc-b333-ae0c656a49b1";
 const ENABLE_SERVICE_UUID     = "f3e031b2-f057-4dbc-917d-8cacf6e78234";
 const ENABLE_FLAG_UUID        = "17603fac-2e15-4afd-962d-107464389c5a";
+
 $(function() {
+
 	$("#connect").click(() => {
 
 		navigator.bluetooth.requestDevice({ 
-			filters: [ { name: DEVICE_NAME } ] 
+			filters: [ { services: ENABLE_SERVICE_UUID } ] 
 		})
 		.then( device => device.gatt.connect() )
 		.then( server => server.getPrimaryService( ENABLE_SERVICE_UUID ) )
@@ -23,6 +25,7 @@ $(function() {
 		.then(_ => {
 	  		console.log( 'Ox Enabled' );
 		})
+
 		.catch(error => { console.log(error); });
 	})
 })
